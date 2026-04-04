64d0772984addb3d9dafa6e09b3cc72e64a306d3
Douglas Alves <rachebartm00s@proton.me>
2026-04-04 21:14:54 +0100

    setting up nginx confs and dockerfiles

---
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir -p /etc/nginx/ssl
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+	-keyout /etc/nginx/ssl/key.pem \
+	-out /etc/nginx/ssl/cert.pem \
+	-subj "/CN=dopereir.42.fr"
+
+
+# req -> certificate request command
+# -x509 -> skip request and generate a self-signed cert
+# -nodes -> set the key as not password protected, allows nginx
+#	to start without ask for password
+# -days 365, cert expire in one year, someone says that is a good pratic
+# -newkey rsa:2048 -> RSA key of 2048bits
